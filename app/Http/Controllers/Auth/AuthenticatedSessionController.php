e46bab2e00aa5c7997a48caf3f412a18675035a8
NayeliRamLop <jessicanayeliuwu@gmail.com>
2026-03-17 19:53:43 -0600

    agrega redireccion admin en auth

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -50,6 +50,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (Auth::user()->isAdmin()) {
+            return redirect()->route('admin.users.index');
+        }
+
         return redirect()->intended('home');
     }
 
